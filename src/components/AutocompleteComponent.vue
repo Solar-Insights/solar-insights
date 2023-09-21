863a829b8cf6eb4efc7160eb12784a651b751e4e
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2023-09-20 23:24:54 -0400

    Added models and geocoding for autocomplete Changed terminology in router

--- src/components/AutocompleteComponent.vue
+++ src/components/AutocompleteComponent.vue
@@ -1,15 +1,18 @@
 <template>
     <input
-      id="pac-input"
-      type="text"
-      placeholder="Search Box"
-      style="width: 100%"
+        id="autocomplete-input"
+        type="text"
+        placeholder="Search Box"
+        style="width: 100%"
     />
+    <v-btn @click="console.log(getGeocoding())"> Enter </v-btn>
 </template>
 
 <script setup lang="ts">
 // Vue
 import { onMounted } from "vue";
+// Objects
+import { coordinates } from "@/objects/models";
 
 onMounted(async () => {
     let autocomplete: google.maps.places.Autocomplete = await initAutocomplete();
@@ -17,7 +20,7 @@ onMounted(async () => {
 
 async function initAutocomplete(): Promise<google.maps.places.Autocomplete> {
     const { Autocomplete } = await google.maps.importLibrary("places") as google.maps.PlacesLibrary;
-    const input = document.getElementById("pac-input") as HTMLInputElement;
+    const input = document.getElementById("autocomplete-input") as HTMLInputElement;
     const options = {
         componentRestrictions: { country: "ca" },
         fields: ["formatted_address"],
@@ -26,4 +29,17 @@ async function initAutocomplete(): Promise<google.maps.places.Autocomplete> {
     return new Autocomplete(input, options);
 }
 
+async function getGeocoding(): Promise<coordinates> {
+    const input = document.getElementById("autocomplete-input") as HTMLInputElement;
+    const geocoder = new google.maps.Geocoder()
+    const options = {
+        address: input.value,
+        language: "fr-CA"
+    };
+    const geocodingRequest: google.maps.GeocoderResponse = await geocoder.geocode(options);
+    return {
+        lat: geocodingRequest.results[0].geometry.location.lat(),
+        lng: geocodingRequest.results[0].geometry.location.lng()
+    };
+}
 </script>
