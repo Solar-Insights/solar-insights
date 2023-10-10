32b22f54b43cf0adc041c5aa21a80199e7c71a11
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2023-10-10 02:21:00 -0400

    Added a label only map Added restrictions for map drag and drop

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="map" style="width: 100%; height: 100%"></div>
+    <div id="map" style="width: 100%; height: 100%;"></div>
 </template>
 
 <script setup lang="ts">
@@ -10,15 +10,15 @@ import { useRoute } from "vue-router";
 // Models
 import { coordinates, validCoordinates } from "@/models/models";
 
-
 var airQualityType = "UAQI_INDIGO_PERSIAN";
-
 
 onMounted(async () => {
     const coord: coordinates = getCoordinates();
     const map = await initMap(coord);
-    const layer = await initAirQualityLayer();
+    const labelOnlyMap = await initLabelOnlyMap();
+    const layer = initAirQualityLayer();
     map.overlayMapTypes.push(layer as google.maps.MapType);
+    map.overlayMapTypes.push(labelOnlyMap as google.maps.MapType);
 });
 
 async function initMap(coord: coordinates): Promise<google.maps.Map> {
@@ -29,30 +29,60 @@ async function initMap(coord: coordinates): Promise<google.maps.Map> {
             mapId: import.meta.env.VITE_AIR_QUALITY_MAP_ID,
             center: { lat: coord.lat, lng: coord.lng },
             zoom: 4,
-            minZoom: 3,
+            minZoom: 1,
             maxZoom: 6,
             clickableIcons: false,
             disableDoubleClickZoom: false,
             isFractionalZoomEnabled: false,
             keyboardShortcuts: false,
             streetViewControl: false,
-            mapTypeControl: false
+            mapTypeControl: false,
+            restriction: {
+                latLngBounds: {north: 82, south: -82, west: -180, east: 180},
+                strictBounds: true
+            },
         }
     );
 }
 
-async function initAirQualityLayer() {
+function initAirQualityLayer() {
     return new google.maps.ImageMapType(
         {
             getTileUrl: function(coord, zoom) {
-                var airQualityUrl = `https://airquality.googleapis.com/v1/mapTypes/${airQualityType}/heatmapTiles/${zoom}/${coord.x}/${coord.y}?key=${import.meta.env.VITE_GOOGLE_API}`;
-                return airQualityUrl;
+                console.log("fetching")
+                return `https://airquality.googleapis.com/v1/mapTypes/${airQualityType}/heatmapTiles/${zoom}/${coord.x}/${coord.y}?key=${import.meta.env.VITE_GOOGLE_API}`;
             },
-            minZoom: 3,
+            minZoom: 1,
             maxZoom: 6,
             tileSize: new google.maps.Size(256, 256),
             opacity: 0.75
         }
+    );
+}
+
+function initLabelOnlyMap() {
+    return new google.maps.StyledMapType(
+        [
+            {
+                stylers: [{
+                    visibility: 'off'
+                }]
+            }, 
+            {
+                elementType: 'labels',
+                stylers: [
+                    { color: '#303b45' },
+                    { visibility: 'on' },
+                    { saturation: -100 }
+                ]
+            },
+            {
+                elementType: 'labels.text.stroke',
+                stylers: [
+                    { color: "#FFFFFF"}
+                ]
+            }
+        ],
     );
 }
 
