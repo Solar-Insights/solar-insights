237b5229437fd8b2a54d284addb55c8f25741df6
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2023-10-14 03:06:45 -0400

    Removed unused functions Init functions moved into file for clarity

---
+++ src/plugins/googleMapsAPI.ts
@@ -0,0 +1,102 @@
+// Models
+import { coordinates } from "@/models/models";
+
+export async function initMap(coord: coordinates, mapElement: HTMLElement): Promise<google.maps.Map> {
+    const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+    return new Map(
+        mapElement,
+        {
+            mapTypeId: google.maps.MapTypeId.SATELLITE,
+            center: { lat: coord.lat, lng: coord.lng },
+            zoom: 15,
+            minZoom: 15,
+            maxZoom: 15,
+            clickableIcons: false,
+            disableDoubleClickZoom: false,
+            isFractionalZoomEnabled: false,
+            keyboardShortcuts: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+            zoomControl: false,
+            scrollwheel: false,
+            rotateControl: false,
+            restriction: {
+                latLngBounds:{
+                    north: 85.0, 
+                    south: -60.0, 
+                    west: -179.0, 
+                    east: 179.0
+                },
+                strictBounds : true
+            }
+        }
+    );
+}
+
+export function addMarker(coord: coordinates, map: google.maps.Map) {
+    const marker = new google.maps.Marker({
+        position: { lat: coord.lat, lng: coord.lng },
+        title:"Hello World!"
+    });
+    marker.setMap(map);
+}
+
+export function initLabelOnlyMap() {
+    return new google.maps.StyledMapType(
+        [
+            {
+                stylers: [{
+                    visibility: 'off'
+                }]
+            }, 
+            {
+                featureType: 'administrative',
+                elementType: 'labels',
+                stylers: [
+                    { visibility: 'on' },
+                    { color: '#55595C' }
+                ]
+            },
+            {
+                featureType: 'administrative',
+                elementType: 'labels.text.stroke',
+                stylers: [
+                    { color: "#FFFFFF"}
+                ]
+            }
+        ],
+    );
+}
+
+export async function initAutocomplete(): Promise<google.maps.places.Autocomplete> {
+    const { Autocomplete } = await google.maps.importLibrary("places") as google.maps.PlacesLibrary;
+    const input = document.getElementById("autocomplete-search") as HTMLInputElement;
+    const options = {
+        fields: ["formatted_address"],
+        types: ["address"]
+    };
+    return new Autocomplete(input, options);
+}
+
+export async function getGeocoding(formattedAddress: string): Promise<coordinates> {
+    console.log("get lat and lng from address");
+    const geocoder = new google.maps.Geocoder()
+    const options = {
+        address: formattedAddress
+    };
+    const coord: coordinates = {
+        lat: 0,
+        lng: 0
+    };
+    await geocoder.geocode(options)
+        .then((geocodingRequest: google.maps.GeocoderResponse) => {
+            coord.lat = geocodingRequest.results[0].geometry.location.lat();
+            coord.lng = geocodingRequest.results[0].geometry.location.lng();
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+
+    return coord;
+}
