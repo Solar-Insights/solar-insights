f912ee1a106f60c120df8939f625613964b1dc69
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-01-09 14:41:50 -0500

    Adding formulas to measure solar values

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -1,3 +1,55 @@
+// Models
+import { UserSolarData } from "@/models/models";
+
+/*
+    Units: None
+*/
+export function panelCapacityRatioCalc(userSolarData: UserSolarData) {
+    return userSolarData.panelCapacityWatts / userSolarData.defaultPanelCapacityWatts;
+}
+
+/*
+    Units: kWh
+*/
+export function yearlyEnergyCalc(userSolarData: UserSolarData) {
+    return userSolarData.yearlyEnergyDcKwh * panelCapacityRatioCalc(userSolarData);
+}
+
+/*
+    Units: kW
+*/
+export function installationSizeCalc(userSolarData: UserSolarData) {
+    return userSolarData.panelCount * userSolarData.panelCapacityWatts / 1000;
+}
+
+/*
+    Units: $
+*/
+export function installationCostCalc(userSolarData: UserSolarData) {
+    return userSolarData.installationCostPerWatt * installationSizeCalc(userSolarData) * 1000;
+}
+
+/*
+    Units: kWh
+*/
+export function yearlyEnergyAcProductionKwh(userSolarData: UserSolarData){
+    return userSolarData.yearlyEnergyDcKwh * panelCapacityRatioCalc(userSolarData) * userSolarData.dcToAcDerate;
+}
+
+/*
+    Units: kWh
+*/
+export function yearlyEnergyConsumptionKwh(userSolarData: UserSolarData) {
+    return userSolarData.averageMonthlyEnergyBill / userSolarData.energyCostPerKwh * 12;
+}
+
+/*
+    Units: %
+*/
+export function energyCoveredCalc(userSolarData: UserSolarData) {
+    return yearlyEnergyAcProductionKwh(userSolarData) / yearlyEnergyConsumptionKwh(userSolarData);
+}
+
 export function circularBarColorSelector(value: string) {
     const num_value: number = Number(value);
     if ( isNaN(num_value)) return "green";
