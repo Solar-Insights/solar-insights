454ad4706a79b3acd99d267ae2434cce2d725df2
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-02-09 14:49:16 -0500

    Mathis feat: getSolarLayers call

--- src/server/solar.ts
+++ src/server/solar.ts
@@ -3,7 +3,7 @@ import { Coordinates } from 'solar-typing/src/general';
 
 axios.defaults.baseURL = import.meta.env.VITE_DEV_URL;
 
-export async function getClosestBuildingInsights(coord: Coordinates) {
+export async function getClosestBuildingInsights(coord: Coordinates, radius: number) {
     await axios({
         method: "get",
         responseType: 'json',
@@ -11,6 +11,25 @@ export async function getClosestBuildingInsights(coord: Coordinates) {
         params: {
             lat: coord.lat,
             lng: coord.lng
+        }
+      })
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+export async function getSolarLayers(coord: Coordinates, radius: number) {
+    await axios({
+        method: "get",
+        responseType: 'json',
+        url: `/solar/solar-layers`,
+        params: {
+            lat: coord.lat,
+            lng: coord.lng,
+            radius: radius
         }
       })
         .then((response) => {
