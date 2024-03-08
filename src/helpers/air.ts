88209b2c154246615af4b67f35809bfc82ff304e
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-03-08 17:46:32 -0500

    Mathis fix: makeDominantPollutantFirst is now properly working

--- src/helpers/air.ts
+++ src/helpers/air.ts
@@ -1,12 +1,10 @@
 import { AirPollutant } from "geo-env-typing/air";
 
 export function makeDominantPollutantFirst(dominantPollutant: string, listOfPollutants: AirPollutant[]) {
-    const index = listOfPollutants.findIndex((pollutant) => pollutant.code == dominantPollutant);
+    const index = listOfPollutants.findIndex((pollutant) => pollutant.code === dominantPollutant);
     if (index != -1) {
-        listOfPollutants.sort((a, b) => {
-            if (a.code == dominantPollutant) return -1;
-            if (b.code == dominantPollutant) return -1;
-            return a.code.localeCompare(b.code);
-        });
+        const tempoPollutant = listOfPollutants[index];
+        listOfPollutants.splice(index, 1);
+        listOfPollutants.unshift(tempoPollutant);
     }
 }
