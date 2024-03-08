d5d2206ca9b99c6738bb7fe02dfca18c7c10e0b1
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-03-08 13:11:45 -0500

    Mathis feat: App bar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
     <v-app>
+        <app-bar/>
         <v-main>
             <v-container class="app-container" fluid>
-                <AlertComponent />
+                <alert-component/>
                 <router-view :key="$route.fullPath"></router-view>
             </v-container>
         </v-main>
@@ -12,4 +13,10 @@
 <script setup lang="ts">
 // Components
 import AlertComponent from "@/components/util/AlertComponent.vue";
+import AppBar from "@/components/util/AppBar.vue";
+// Style
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+
 </script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,5 @@
 <template>
     <h1>Home</h1>
-    <v-btn :to="{ name: 'air-quality-map' }"> Carte qualite de l'air </v-btn>
-
-    <v-btn :to="{ name: 'solar-map' }"> Carte intensite du soleil </v-btn>
 </template>
 
 <script setup lang="ts"></script>
