9a002fa3ca64d77ba96fad455698bfd22ad59894
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-03-15 17:31:33 -0400

    Mathis feat: Split the timeseries creation into multiple functions

--- src/helpers/solar.ts
+++ src/helpers/solar.ts
@@ -47,90 +47,6 @@ export function createPalette(hexColors: string[], size = 256) {
         });
 }
 
-export async function drawSolarInsightsChart(userSolarData: UserSolarData, costChart: HTMLElement) {
-    const year = new Date().getFullYear();
-    const yearsList = [];
-    for (let i = year; i < year + userSolarData.installationLifespan; i++) {
-        const currentYear = new Date();
-        currentYear.setFullYear(i);
-        yearsList.push(currentYear);
-    }
-
-    let costWithSolar = 0;
-    const utilityBillEstimates: number[] = yearlyUtilityBillEstimates(userSolarData);
-    const cumulativeCostWithSolar: [Date, number][] = [];
-    for (let i = 0; i < userSolarData.installationLifespan; i++) {
-        costWithSolar +=
-            i == 0
-                ? utilityBillEstimates[i] + installationCostCalc(userSolarData) - userSolarData.solarIncentives
-                : utilityBillEstimates[i];
-        cumulativeCostWithSolar.push([yearsList[i], Math.round(costWithSolar)]);
-    }
-
-    let costWithoutSolar = 0;
-    const yearlyCostWithoutSolarEstimates: number[] = yearlyCostWithoutSolar(userSolarData);
-    const cumulativeCostWithoutSolar: [Date, number][] = [];
-    for (let i = 0; i < userSolarData.installationLifespan; i++) {
-        costWithoutSolar += yearlyCostWithoutSolarEstimates[i];
-        cumulativeCostWithoutSolar.push([yearsList[i], Math.round(costWithoutSolar)]);
-    }
-
-    const options = {
-        chart: {
-          type: 'line',
-          zoom: {
-            enabled: false
-          }
-        },
-        series: [
-            {
-                name: 'Solar',
-                data: cumulativeCostWithSolar
-            },
-            {
-                name: 'Current',
-                data: cumulativeCostWithoutSolar
-            },
-        ],
-        grid: {
-            show: false
-        },
-        xaxis: {
-            type: "datetime",
-            axisBorder: {
-                show: true
-            },
-            axisTicks: {
-                show: true
-            }
-        },
-        yaxis: {
-            axisBorder: {
-                show: true
-            },
-            axisTicks: {
-                show: true
-            },
-            labels: {
-                formatter: function (value: any) {
-                    return strToLargeNumberDisplay(value) + "$";
-                }
-            }
-        },
-        tooltip: {
-            x: {
-                format: "yyyy"
-            },
-        },
-        legend: {
-            position: 'top',
-            horizontalAlign: 'left'
-        }
-      }
-      
-    return new ApexCharts(costChart, options);
-}
-
 export async function getSingleLayer(layerId: LayerId, urls: SolarLayers) {
     const get: Record<LayerId, () => Promise<Layer>> = {
         annualFlux: async () => {
@@ -366,4 +282,112 @@ export function makeDefaultTimeParams() {
         day: 1,
         hour: 0
     } as TimeParameters;
+}
+
+export async function drawSolarInsightsChart(userSolarData: UserSolarData, costChart: HTMLElement) {
+    const options = {
+        chart: {
+          type: 'line',
+          zoom: {
+            enabled: false
+          }
+        },
+        series: makeTimeSeriesFromUserSolarData(userSolarData),
+        grid: {
+            show: false
+        },
+        xaxis: {
+            type: "datetime",
+            axisBorder: {
+                show: true
+            },
+            axisTicks: {
+                show: true
+            }
+        },
+        yaxis: {
+            axisBorder: {
+                show: true
+            },
+            axisTicks: {
+                show: true
+            },
+            labels: {
+                formatter: function (value: any) {
+                    return strToLargeNumberDisplay(value) + "$";
+                }
+            }
+        },
+        tooltip: {
+            x: {
+                format: "yyyy"
+            },
+        },
+        legend: {
+            position: 'top',
+            horizontalAlign: 'left'
+        }
+      }
+      
+    return new ApexCharts(costChart, options);
+}
+
+function makeTimeSeriesFromUserSolarData(userSolarData: UserSolarData) {
+    const yearsList: Date[] = makeYearsList(userSolarData);
+    const cumulativeCostWithSolar: [Date, number][] = makeCumulativeCostWithSolar(userSolarData, yearsList);
+    const cumulativeCostWithoutSolar: [Date, number][] = makeCumulativeCostWithoutSolar(userSolarData, yearsList);
+
+    return [
+        {
+            name: 'Solar',
+            data: cumulativeCostWithSolar
+        },
+        {
+            name: 'Current',
+            data: cumulativeCostWithoutSolar
+        },
+    ]
+}
+
+function makeYearsList(userSolarData: UserSolarData) {
+    const currentYear = new Date().getFullYear();
+    const endOfLifespanYear = currentYear + userSolarData.installationLifespan;
+    const yearsList = [];
+
+    for (let i = currentYear; i < endOfLifespanYear; i++) {
+        const currentYear = new Date();
+        currentYear.setFullYear(i);
+        yearsList.push(currentYear);
+    }
+
+    return yearsList
+}
+
+function makeCumulativeCostWithSolar(userSolarData: UserSolarData, yearsList: Date[]) {
+    const utilityBillEstimates: number[] = yearlyUtilityBillEstimates(userSolarData);
+    const cumulativeCostWithSolar: [Date, number][] = [];
+    let costWithSolar = 0;
+
+    for (let i = 0; i < userSolarData.installationLifespan; i++) {
+        costWithSolar +=
+            i == 0
+                ? utilityBillEstimates[i] + installationCostCalc(userSolarData) - userSolarData.solarIncentives
+                : utilityBillEstimates[i];
+        cumulativeCostWithSolar.push([yearsList[i], Math.round(costWithSolar)]);
+    }
+
+    return cumulativeCostWithSolar;
+}
+
+function makeCumulativeCostWithoutSolar(userSolarData: UserSolarData, yearsList: Date[]) {
+    const yearlyCostWithoutSolarEstimates: number[] = yearlyCostWithoutSolar(userSolarData);
+    const cumulativeCostWithoutSolar: [Date, number][] = [];
+    let costWithoutSolar = 0;
+
+    for (let i = 0; i < userSolarData.installationLifespan; i++) {
+        costWithoutSolar += yearlyCostWithoutSolarEstimates[i];
+        cumulativeCostWithoutSolar.push([yearsList[i], Math.round(costWithoutSolar)]);
+    }
+
+    return cumulativeCostWithoutSolar;
 }
