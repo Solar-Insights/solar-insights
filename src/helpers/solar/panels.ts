037793cf153e0caf7e522b4906c8ed9adb26898e
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-03-18 21:06:10 -0400

    Mathis fix: refactor of functions and move to solar files

---
+++ src/helpers/solar/panels.ts
@@ -0,0 +1,45 @@
+import { normalize } from "@/helpers/solar/solarDataMath";
+import { panelsPalette } from "@/helpers/constants";
+import { rgbToColor, createPalette } from "@/helpers/solar/colorPalettes";
+import { BuildingInsights } from "geo-env-typing/solar";
+
+export async function createSolarPanelsFromBuildingInsights(buildingInsights: BuildingInsights) {
+    const geometryLib: google.maps.GeometryLibrary = await google.maps.importLibrary("geometry") as google.maps.GeometryLibrary;
+    const solarPotential = buildingInsights.solarPotential;
+
+    const palette = createPalette(panelsPalette, 256).map(rgbToColor);
+    const minEnergy = solarPotential.solarPanels.slice(-1)[0].yearlyEnergyDcKwh;
+    const maxEnergy = solarPotential.solarPanels[0].yearlyEnergyDcKwh;
+
+    const solarPanels: google.maps.Polygon[] = solarPotential.solarPanels.map((panel) => {
+        const [w, h] = [solarPotential.panelWidthMeters / 2, solarPotential.panelHeightMeters / 2];
+        const points = [
+            { x: +w, y: +h }, // top right
+            { x: +w, y: -h }, // bottom right
+            { x: -w, y: -h }, // bottom left
+            { x: -w, y: +h }, // top left
+            { x: +w, y: +h } //  top right
+        ];
+        const orientation = panel.orientation == "PORTRAIT" ? 90 : 0;
+        const azimuth = solarPotential.roofSegmentStats[panel.segmentIndex].azimuthDegrees;
+        const colorIndex = Math.round(normalize(panel.yearlyEnergyDcKwh, maxEnergy, minEnergy) * 255);
+        return new google.maps.Polygon({
+            paths: points.map(({ x, y }) =>
+                geometryLib.spherical.computeOffset(
+                    { lat: panel.center.latitude, lng: panel.center.longitude },
+                    Math.sqrt(x * x + y * y),
+                    Math.atan2(y, x) * (180 / Math.PI) + orientation + azimuth
+                )
+            ),
+            strokeColor: "#B0BEC5",
+            strokeOpacity: 0.9,
+            strokeWeight: 1,
+            fillColor: palette[colorIndex],
+            fillOpacity: 0.9
+        });
+    });
+
+    return solarPanels;
+}
+
+
--- src/stores/solarMapStore.ts
+++ src/stores/solarMapStore.ts
@@ -8,10 +8,12 @@ import { panelCapacityRatioCalc, dcToAcDerate, yearlyEnergyConsumptionKwh, norma
 import { makeCumulativeCostWithoutSolar, makeCumulativeCostWithSolar } from "@/helpers/solar/yearlyAndCumulativeCosts"
 import { SolarReadonlyPanel, TimeParameters, UserSolarData } from "@/helpers/types";
 import { panelsPalette } from "@/helpers/constants";
-import { rgbToColor, createPalette, getSingleLayer } from "@/helpers/solar/colorsAndLayers";
+import { rgbToColor, createPalette } from "@/helpers/solar/colorPalettes";
+import { getSingleLayer } from "@/helpers/solar/geotiffs";
 import { makeDefaultUserSolarDataObject, makeDefaultMapSettings, makeDefaultTimeParams, } from "@/helpers/solar/defaultData";
 // Server
 import { getClosestBuildingInsights, getSolarLayers } from "@/server/solar";
+import { createSolarPanelsFromBuildingInsights } from "@/helpers/solar/panels";
 
 export const useSolarMapStore = defineStore("solarMapStore", {
     state: () => ({
@@ -146,40 +148,8 @@ export const useSolarMapStore = defineStore("solarMapStore", {
         },
 
         async addSolarPanelsToMap() {
-            const geometryLib = await this.geometryLibrary;
-            const solarPotential = this.buildingInsights.solarPotential;
-            const palette = createPalette(panelsPalette, 256).map(rgbToColor);
-            const minEnergy = solarPotential.solarPanels.slice(-1)[0].yearlyEnergyDcKwh;
-            const maxEnergy = solarPotential.solarPanels[0].yearlyEnergyDcKwh;
-
-            this.solarPanels = solarPotential.solarPanels.map((panel) => {
-                const [w, h] = [solarPotential.panelWidthMeters / 2, solarPotential.panelHeightMeters / 2];
-                const points = [
-                    { x: +w, y: +h }, // top right
-                    { x: +w, y: -h }, // bottom right
-                    { x: -w, y: -h }, // bottom left
-                    { x: -w, y: +h }, // top left
-                    { x: +w, y: +h } //  top right
-                ];
-                const orientation = panel.orientation == "PORTRAIT" ? 90 : 0;
-                const azimuth = solarPotential.roofSegmentStats[panel.segmentIndex].azimuthDegrees;
-                const colorIndex = Math.round(normalize(panel.yearlyEnergyDcKwh, maxEnergy, minEnergy) * 255);
-                return new google.maps.Polygon({
-                    paths: points.map(({ x, y }) =>
-                        geometryLib.spherical.computeOffset(
-                            { lat: panel.center.latitude, lng: panel.center.longitude },
-                            Math.sqrt(x * x + y * y),
-                            Math.atan2(y, x) * (180 / Math.PI) + orientation + azimuth
-                        )
-                    ),
-                    strokeColor: "#B0BEC5",
-                    strokeOpacity: 0.9,
-                    strokeWeight: 1,
-                    fillColor: palette[colorIndex],
-                    fillOpacity: 0.9
-                });
-            });
-
+            this.solarPanels = await createSolarPanelsFromBuildingInsights(this.buildingInsights);
+            
             this.solarPanels.map((panel, i) =>
                 panel.setMap(
                     this.mapSettings.showPanels && this.panelConfig && i < this.panelConfig.panelsCount
@@ -192,6 +162,7 @@ export const useSolarMapStore = defineStore("solarMapStore", {
         async panelCountChange() {
             this.userSolarData.panelCount =
                 this.buildingInsights.solarPotential.solarPanelConfigs[this.mapSettings.configIdIndex].panelsCount;
+
             await this.syncMapWithNewRequest();
         },
 
@@ -239,19 +210,27 @@ export const useSolarMapStore = defineStore("solarMapStore", {
         },
 
         resetHeatmapLayer() {
-            if (!this.layer) {
+            this.renderOverlay();
+            this.displayCorrectLayer();
+        },
+
+        renderOverlay() {
+            if (!this.layer || !this.mapSettings.showHeatmap) {
                 return;
             }
 
             const bounds = this.layer.bounds;
-            this.overlays.map((overlay) => overlay.setMap(null));
-            if (!this.mapSettings.showHeatmap) {
-                return;
-            }
 
+            this.overlays.map((overlay) => overlay.setMap(null));
             this.overlays = this.layer
                 .render(this.showRoofOnly, this.timeParams.month, this.timeParams.day)
                 .map((canvas) => new google.maps.GroundOverlay(canvas.toDataURL(), bounds));
+        },
+
+        displayCorrectLayer() {
+            if (!this.layer) {
+                return;
+            }
 
             if (this.layer.id === "monthlyFlux") {
                 this.displayMonthlyFlux();
@@ -259,7 +238,7 @@ export const useSolarMapStore = defineStore("solarMapStore", {
                 this.displayHourlyShade();
             } else {
                 this.overlays[0].setMap(this.map);
-            }
+            } 
         },
 
         displayMonthlyFlux() {
