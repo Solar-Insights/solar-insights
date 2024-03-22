fea3eed4908019be1e37809052165dc8e5b2a700
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-03-22 19:42:49 -0400

    Mathis feat: Started adding locale

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // Vue
 import { createApp } from "vue";
-import App from "./App.vue";
+import App from "@/App.vue";
 import router from "@/router";
 // Store
 import { createPinia } from "pinia";
@@ -8,10 +8,25 @@ import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 // Style
 import vuetifyThemesAndComponents from "@/vuetify";
 import "@/assets/_main.scss";
+// i19n
+import { createI18n } from "vue-i18n";
+//import {} from "@/i18n/general";
+//import {} from "@/i18n/solar";
+//import {} from "@/i18n/air_quality";
 
-// Create app and add required plugins
+const messages = {
+    fr: { hello: "bonjour" },
+    en: { hello: "hi" }
+}
+const i18n = createI18n({
+    locale: "fr",
+    fallbackLocale: "en",
+    messages
+});
+
 const app = createApp(App);
 
+app.use(i18n);
 app.use(createPinia().use(piniaPluginPersistedState));
 app.use(router);
 app.use(vuetifyThemesAndComponents);
