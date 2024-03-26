593f35f63375bafabc87d6ecf1bcaa3dc4d4b64b
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-03-25 20:23:55 -0400

    Mathis feature: InsightsReadonly i18n

--- src/components/solar/InsightsReadonlyPanel.vue
+++ src/components/solar/InsightsReadonlyPanel.vue
@@ -7,7 +7,7 @@
         <v-card-title class="text-center font-weight-regular pl-0 mb-4">
             <div class="d-flex mb-3">
                 <v-icon class="mr-3" color="theme">mdi-flash-outline</v-icon>
-                <div class="my-auto">Solar Insights</div>
+                <div class="my-auto">{{ $t("solar.insights-readonly.name") }}</div>
             </div>
             <v-divider style="border-width: 2px" />
         </v-card-title>
@@ -15,7 +15,7 @@
         <div class="mb-5">
             <div class="d-flex">
                 <v-icon class="mr-3" color="theme">mdi-calendar-clock-outline</v-icon>
-                <div class="me-auto">Yearly Energy</div>
+                <div class="me-auto">{{ $t("solar.insights-readonly.yearly-energy") }}</div>
                 <div>{{ strToLargeNumberDisplay(yearlyEnergyCalc(userSolarData).toFixed(0)) }} kWh</div>
             </div>
         </div>
@@ -23,7 +23,7 @@
         <div class="mb-5">
             <div class="d-flex">
                 <v-icon class="mr-3" color="theme">mdi-account-cash-outline</v-icon>
-                <div class="me-auto">Installation Cost</div>
+                <div class="me-auto">{{ $t("solar.insights-readonly.installation-cost") }}</div>
                 <div>{{ strToLargeNumberDisplay(installationCostCalc(userSolarData).toFixed(0)) }} $</div>
             </div>
         </div>
@@ -31,7 +31,7 @@
         <div class="mb-5">
             <div class="d-flex">
                 <v-icon class="mr-3" color="theme">{{ batteryCharging(energyCoveredCalc(userSolarData)) }}</v-icon>
-                <div class="me-auto">Energy Covered</div>
+                <div class="me-auto">{{ $t("solar.insights-readonly.energy-covered") }}</div>
                 <div>{{ energyCoveredCalc(userSolarData).toFixed(0) }} %</div>
             </div>
         </div>
@@ -39,14 +39,14 @@
         <div class="mb-5">
             <div>&nbsp</div>
             <div class="d-flex">
-                <div class="me-auto">Cost analysis for {{ userSolarData.installationLifespan }} years</div>
+                <div class="me-auto">{{ $t("solar.insights-readonly.cost-analysis-for") }} {{ userSolarData.installationLifespan }} {{ $t("solar.insights-readonly.years") }}</div>
             </div>
         </div>
 
         <div class="mb-5">
             <div class="d-flex">
                 <v-icon class="mr-3" color="theme">mdi-transmission-tower</v-icon>
-                <div class="me-auto">Cost without solar panels</div>
+                <div class="me-auto">{{ $t("solar.insights-readonly.cost-without") }}</div>
                 <div>{{ strToLargeNumberDisplay(costWithoutSolarInstallation(userSolarData).toFixed(0)) }} $</div>
             </div>
         </div>
@@ -54,7 +54,7 @@
         <div class="mb-5">
             <div class="d-flex">
                 <v-icon class="mr-3" color="theme">mdi-solar-panel</v-icon>
-                <div class="me-auto">Cost with solar panels</div>
+                <div class="me-auto">{{ $t("solar.insights-readonly.cost-with") }}</div>
                 <div>{{ strToLargeNumberDisplay(costWithSolarInstallation(userSolarData).toFixed(0)) }} $</div>
             </div>
         </div>
@@ -62,7 +62,7 @@
         <div class="mb-5">
             <div class="d-flex">
                 <v-icon class="mr-3" color="theme">mdi-bank-outline</v-icon>
-                <div class="me-auto">Total saved cost</div>
+                <div class="me-auto">{{ $t("solar.insights-readonly.total-saved-cost") }}</div>
                 <div>
                     {{
                         strToLargeNumberDisplay(
@@ -79,12 +79,12 @@
         <div class="mb-5">
             <div class="d-flex">
                 <v-icon class="mr-3" color="theme">mdi-scale-balance</v-icon>
-                <div class="me-auto">Break even</div>
+                <div class="me-auto">{{ $t("solar.insights-readonly.break-even") }}</div>
                 <div>
                     <div v-if="breakEvenYear != -1">
-                        {{ breakEvenYear + new Date().getFullYear() }} in {{ breakEvenYear }} years
+                        {{ breakEvenYear + new Date().getFullYear() }} ({{ breakEvenYear }} {{ $t("solar.insights-readonly.years") }})
                     </div>
-                    <div v-if="breakEvenYear == -1">- years</div>
+                    <div v-if="breakEvenYear == -1">- {{ $t("solar.insights-readonly.years") }}</div>
                 </div>
             </div>
         </div>
