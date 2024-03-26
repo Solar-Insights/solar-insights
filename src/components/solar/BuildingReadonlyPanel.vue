d0e264fc63963e6342bf30ffc361fc9f5ed28a40
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-03-25 20:30:22 -0400

    Mathis feat: BuildingReadonly i18n

--- src/components/solar/BuildingReadonlyPanel.vue
+++ src/components/solar/BuildingReadonlyPanel.vue
@@ -7,7 +7,7 @@
         <v-card-title class="text-center font-weight-regular pl-0 mb-4">
             <div class="d-flex mb-3">
                 <v-icon class="mr-3" color="theme">mdi-home-outline</v-icon>
-                <div class="my-auto">Building Attributes</div>
+                <div class="my-auto">{{ $t("solar.building-readonly.name") }}</div>
             </div>
             <v-divider />
         </v-card-title>
@@ -15,7 +15,7 @@
         <div class="mb-5">
             <div class="d-flex">
                 <v-icon class="mr-3" color="theme">mdi-math-compass</v-icon>
-                <div class="me-auto">Roof area</div>
+                <div class="me-auto">{{ $t("solar.building-readonly.roof-area") }}</div>
                 <div>
                     {{
                         strToLargeNumberDisplay(buildingInsights?.solarPotential.wholeRoofStats.areaMeters2.toFixed(0))
@@ -23,35 +23,35 @@
                     m²
                 </div>
             </div>
-            <div class="detail-text mt-1" style="margin-left: 33.6px">Based on 3D analysis of the roof</div>
+            <div class="detail-text mt-1" style="margin-left: 33.6px">{{ $t("solar.building-readonly.based-on") }}</div>
         </div>
 
         <div class="mb-5">
             <div class="d-flex">
                 <v-icon class="mr-3" color="theme">mdi-sun-clock-outline</v-icon>
-                <div class="me-auto flex-shrink-1">Annual sunlight</div>
+                <div class="me-auto flex-shrink-1">{{ $t("solar.building-readonly.annual-sunlight") }}</div>
                 <div>
                     {{ strToLargeNumberDisplay(buildingInsights?.solarPotential.maxSunshineHoursPerYear.toFixed(0)) }}
-                    hrs
+                    {{ $t("solar.building-readonly.hours-abbreviation") }}
                 </div>
             </div>
             <div class="detail-text mt-1" style="margin-left: 33.6px">
-                Derived from daily examination of weather patterns
+                {{ $t("solar.building-readonly.derived-from") }}
             </div>
         </div>
 
         <div class="mb-5">
             <div class="d-flex">
                 <v-icon class="mr-3" color="theme">mdi-solar-power-variant-outline</v-icon>
-                <div class="me-auto">Panels</div>
-                <div>{{ userSolarData.panelCount }} / {{ userSolarData.maxPanelCount }} panels</div>
+                <div class="me-auto">{{ $t("solar.building-readonly.panels-caps") }}</div>
+                <div>{{ userSolarData.panelCount }} / {{ userSolarData.maxPanelCount }} {{ $t("solar.building-readonly.panels") }}</div>
             </div>
         </div>
 
         <div class="mb-5">
             <div class="d-flex">
                 <v-icon class="mr-3" color="theme">mdi-account-hard-hat-outline</v-icon>
-                <div class="me-auto">Installation Size</div>
+                <div class="me-auto">{{ $t("solar.building-readonly.installation-size") }}</div>
                 <div>{{ strToLargeNumberDisplay(installationSizeCalc(userSolarData).toFixed(1)) }} kW</div>
             </div>
         </div>
