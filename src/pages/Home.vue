ebe61342e7af2b1cdf0f989467284f56904fa9c9
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-06 00:02:35 -0400

    Mathis feat: i18n for home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,19 @@
 <template>
     <v-card class="pt-0 body-container">
         <div class="home-title-container">
-            <div class="home-title"> Drive adoption of solar energy with Solar&nbspInsights </div>
-            <div class="home-title-description"> Easy to use, highly adaptive, and accurate. Observe the viability of solar installations in one single search. </div>
+            <div class="home-title"> {{ $t(`home.home-title-container.title`) }} </div>
+            <div class="home-title-description"> {{ $t(`home.home-title-container.description`) }} </div>
             <div class="home-title-action">
                 <v-card 
                     :to="{ name: 'solar-map' }"
-                    class="pa-8 rounded-lg mx-auto"
-                    style="width: 200px;"
+                    class="pa-6 rounded-lg mx-auto"
+                    style="width: 250px;"
                     elevation="2"
                     color="theme"
                 >
                     <v-row class="pa-0">
                         <v-col class="pa-0" cols="9">
-                            <div style="font-weight: bold;"> Discover potential savings </div>
+                            <div style="font-weight: bold;"> {{ $t(`home.home-title-container.action`) }} </div>
                         </v-col>
                         <v-col class="pa-0" cols="3">
                             <v-icon>mdi-magnify</v-icon>
@@ -27,38 +27,38 @@
         <v-img v-if="theme === 'light'" class="home-main-image" :src="home_main_image_light"/>
 
         <div class="home-subtitle-container">
-            <div class="home-subtitle"> What can you do with Solar&nbspInsights? </div>
+            <div class="home-subtitle"> {{ $t(`home.usecase-subtitle-container.subtitle`) }} </div>
             <div class="home-subtitle-content"> 
                 <v-row>
                     <v-col class="mx-auto px-5" cols="12" xs="12" sm="6" md="3"> 
                         <v-icon class="mb-3 mx-auto" size="x-large">mdi-telescope</v-icon>
-                        <div class="font-weight-bold mb-3"> Visualize accurate solar data </div>
+                        <div class="font-weight-bold mb-3"> {{ $t(`home.usecase-subtitle-container.visualize.title`) }} </div>
                         <div> 
-                            Observe a building's daily, monthly, or yearly sunshine patterns. Plus, get estimates on the optimal placement of solar panels.
+                            {{ $t(`home.usecase-subtitle-container.visualize.description`) }}
                         </div>
                     </v-col>
 
                     <v-col class="mx-auto px-5" cols="12" xs="12" sm="6" md="3"> 
                         <v-icon class="mb-3 mx-auto" size="x-large">mdi-hammer</v-icon>
-                        <div class="font-weight-bold mb-3"> Customize solar installations </div>
+                        <div class="font-weight-bold mb-3"> {{ $t(`home.usecase-subtitle-container.customize.title`) }}  </div>
                         <div> 
-                            Optimize a solar installation to fit your budget, or your needs. You can even adjust settings manually.  
+                            {{ $t(`home.usecase-subtitle-container.customize.description`) }}  
                         </div>
                     </v-col>
 
                     <v-col class="mx-auto px-5" cols="12" xs="12" sm="6" md="3"> 
                         <v-icon class="mb-3 mx-auto" size="x-large">mdi-cash</v-icon>
-                        <div class="font-weight-bold mb-3"> Assess the viability of solar panels </div>
+                        <div class="font-weight-bold mb-3"> {{ $t(`home.usecase-subtitle-container.viability.title`) }}  </div>
                         <div> 
-                            Customize financial parameters to find out the economic value of solar panels over their lifespan.
+                            {{ $t(`home.usecase-subtitle-container.viability.description`) }}
                         </div>
                     </v-col>
 
                     <v-col class="mx-auto px-5" cols="12" xs="12" sm="6" md="3"> 
                         <v-icon class="mb-3 mx-auto" size="x-large">mdi-earth</v-icon>
-                        <div class="font-weight-bold mb-3"> Access globally available data </div>
+                        <div class="font-weight-bold mb-3"> {{ $t(`home.usecase-subtitle-container.global.title`) }} </div>
                         <div> 
-                            Enjoy the availability and precision of public solar data and weather patterns across the globe, ranging from North America to Oceania.
+                            {{ $t(`home.usecase-subtitle-container.global.description`) }}
                         </div>
                     </v-col>
                 </v-row>
