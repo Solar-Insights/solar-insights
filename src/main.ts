807502d2d48ae3a74dd8c5d395b6179e5a90c31b
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-09 21:31:08 -0400

    Mathis feat: Added auth0 to app with default settings

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,17 @@ import { createPinia } from "pinia";
 import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 import vuetifyThemesAndComponents from "@/vuetify";
 import "@/assets/_main.scss";
+import { createAuth0 } from "@auth0/auth0-vue";
 
 const app = createApp(App);
 
+app.use(createAuth0({
+    domain: "dev-ubs32bgn56n1z15q.us.auth0.com",
+    clientId: "2m5fiinQSDYS5Zvw1K5SWJjGkKUSgANi",
+    authorizationParams: {
+        redirect_uri: window.location.origin // will choose http://localhost:5173 or https://solarinsights.ca
+    }
+}));
 app.use(i18n);
 app.use(createPinia().use(piniaPluginPersistedState));
 app.use(router);
