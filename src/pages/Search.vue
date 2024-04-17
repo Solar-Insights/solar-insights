d4d8185d5b0c980f49815b528d98901c257afbb0
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-16 21:04:09 -0400

    Mathis feat: Functionnal template to send user to map from coord

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,7 +1,13 @@
 <template>
-
+    <v-btn @click="sendToMap({ lat: 46.81700274623237, lng: -71.36837761987567 })"> Fetch </v-btn>
 </template>
 
 <script setup lang="ts">
+import router from '@/router';
+import { LatLng } from 'geo-env-typing/geo';
 
+
+function sendToMap(coordinates: LatLng) {
+    router.push({ name: "solar-map", query: coordinates });
+}
 </script>
