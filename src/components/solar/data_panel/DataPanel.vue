2754ffb87e08e1072e70e5fba49d43082c18727e
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-23 17:22:39 -0400

    Mathis feat: Moved optimization section to own component

--- src/components/solar/data_panel/DataPanel.vue
+++ src/components/solar/data_panel/DataPanel.vue
@@ -12,45 +12,7 @@
             </v-expansion-panel>
 
             <v-expansion-panel elevation="0" value="optimization">
-                <v-expansion-panel-title v-ripple="{ class: 'text-theme' }">
-                    <v-icon class="mr-3">mdi-bullseye-arrow</v-icon>
-                    <span class="mr-auto"> {{ $t(`solar.data-panel.data-panels.optimizations.name`) }} </span>
-                </v-expansion-panel-title>
-
-                <v-expansion-panel-text>
-                    <div class="detail-text mb-5">
-                        {{ $t(`solar.data-panel.data-panels.optimizations.description`) }}
-                    </div>
-
-                    <div>
-                        <v-card
-                            @click="solarMapStore.setOptimizedSavingsConfig"
-                            class="optimization-card"
-                            variant="outlined"
-                        >
-                            <v-card-item class="pt-0 px-0" prepend-icon="mdi-currency-usd">
-                                {{ $t(`solar.data-panel.data-panels.optimizations.choices.total-savings.name`) }}
-                            </v-card-item>
-                            <v-card-item class="pa-0 pr-2 detail-text" prepend-icon="null">
-                                {{ $t(`solar.data-panel.data-panels.optimizations.choices.total-savings.description`) }}
-                            </v-card-item>
-                        </v-card>
-                        <v-card
-                            @click="solarMapStore.setOptimizedEnergyCoveredConfig"
-                            class="optimization-card"
-                            variant="outlined"
-                        >
-                            <v-card-item class="pt-0 px-0" prepend-icon="mdi-home-battery">
-                                {{ $t(`solar.data-panel.data-panels.optimizations.choices.energy-covered.name`) }}
-                            </v-card-item>
-                            <v-card-item class="pa-0 pr-2 detail-text" prepend-icon="null">
-                                {{
-                                    $t(`solar.data-panel.data-panels.optimizations.choices.energy-covered.description`)
-                                }}
-                            </v-card-item>
-                        </v-card>
-                    </div>
-                </v-expansion-panel-text>
+                <ExpansionOptimization/>
             </v-expansion-panel>
 
             <v-expansion-panel elevation="0" value="settings">
@@ -137,6 +99,7 @@ import { useSolarMapStore } from "@/stores/solarMapStore";
 import ReadonlyPanelSelector from "@/components/solar/pc/ReadonlyPanelSelector.vue";
 import ExpansionPanels from "@/components/solar/data_panel/ExpansionPanels.vue";
 import ExpansionCosts from "@/components/solar/data_panel/ExpansionCosts.vue";
+import ExpansionOptimization from "@/components/solar/data_panel/ExpansionOptimization.vue";
 
 const solarMapStore = useSolarMapStore();
 
