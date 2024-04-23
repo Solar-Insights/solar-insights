08bb18fe73e070d2d8a1f49dec2d3f038bc383e6
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-23 17:28:28 -0400

    Mathis feat: Moved ReadonlyPanelSelector to data_panel subcomponents & removed unused stuff in main DataPanel comp

--- src/components/solar/data_panel/DataPanel.vue
+++ src/components/solar/data_panel/DataPanel.vue
@@ -24,33 +24,11 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import { storeToRefs } from "pinia";
-import { useSolarMapStore } from "@/stores/solarMapStore";
-import ReadonlyPanelSelector from "@/components/solar/pc/ReadonlyPanelSelector.vue";
+import ReadonlyPanelSelector from "@/components/solar/data_panel/ReadonlyPanelSelector.vue";
 import ExpansionPanels from "@/components/solar/data_panel/ExpansionPanels.vue";
 import ExpansionCosts from "@/components/solar/data_panel/ExpansionCosts.vue";
 import ExpansionOptimization from "@/components/solar/data_panel/ExpansionOptimization.vue";
 import ExpansionSettings from "@/components/solar/data_panel/ExpansionSettings.vue";
 
-const solarMapStore = useSolarMapStore();
-
-const { buildingInsights, mapSettings, userSolarData } = storeToRefs(solarMapStore);
-
-const advancedSettingsPanels = ref([] as string[]);
-const advancedSettingsSolarPotential = ref([] as string[]);
 const expansionPanels = ref(["settings"]);
-
-const emits = defineEmits(["showBuildingReadonlyPanel", "showInsightsReadonlyPanel"]);
-
-async function syncMapWithPanels() {
-    await solarMapStore.syncMapWithNewRequest();
-}
-
-async function showDataLayerOnLayerIdChange() {
-    await solarMapStore.showDataLayer(true);
-}
-
-async function changeLayersOnShowHeatmapChange() {
-    mapSettings.value.showHeatmap ? await solarMapStore.showDataLayer(true) : solarMapStore.resetHeatmapLayer();
-}
 </script>
