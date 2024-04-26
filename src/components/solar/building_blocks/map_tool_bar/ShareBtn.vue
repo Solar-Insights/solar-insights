5a4c2fe11320cbb3b6014825f8b64582e6584900
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-26 17:55:39 -0400

    Mathis feat: Sharebtn now properly working

--- src/components/solar/building_blocks/map_tool_bar/ShareBtn.vue
+++ src/components/solar/building_blocks/map_tool_bar/ShareBtn.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-btn icon>
+    <v-btn @click="copyRouteToClipboard()" icon>
         <v-icon>mdi-share</v-icon>
         <Tooltip :message="t(`solar.data-panel.header.toolbar.share-tooltip`)"/>
     </v-btn>
@@ -8,6 +8,12 @@
 <script setup lang="ts">
 import Tooltip from '@/components/general/Tooltip.vue';
 import { useI18n } from 'vue-i18n';
+import { copyToClipboard } from '@/helpers/util';
 
 const t = useI18n().t;
+
+function copyRouteToClipboard() {
+    const fullCurrentRoute = window.location.href;
+    copyToClipboard(fullCurrentRoute);
+}
 </script>
