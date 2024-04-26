f1014d6f3b9145f5e47190635f6cf12aaac12b7c
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-26 16:38:10 -0400

    Mathis feat: Moved save btn to own comp. Will implement later.

--- src/components/solar/building_blocks/MapToolBar.vue
+++ src/components/solar/building_blocks/MapToolBar.vue
@@ -1,21 +1,13 @@
 <template>
-        <v-toolbar density="compact">
-            <SearchBtn/>
+    <v-toolbar density="compact">
+        <SearchBtn/>
+        <SaveBtn />
+        <ShareBtn />
 
-            <v-btn icon>
-                <v-icon>mdi-file-document-plus-outline</v-icon>
-            </v-btn>
+        <v-spacer />
 
-            <v-btn icon>
-                <v-icon>mdi-code-json</v-icon>
-            </v-btn>
-
-            <ShareBtn />
-
-            <v-spacer />
-
-            <SettingsBtn />
-        </v-toolbar>
+        <SettingsBtn />
+    </v-toolbar>
 </template>
 
  <style scoped>
@@ -26,6 +18,7 @@
 
 <script setup lang="ts">
 import SearchBtn from '@/components/solar/building_blocks/map_tool_bar/SearchBtn.vue';
+import SaveBtn from '@/components/solar/building_blocks/map_tool_bar/SaveBtn.vue';
 import ShareBtn from '@/components/solar/building_blocks/map_tool_bar/ShareBtn.vue';
 import SettingsBtn from '@/components/solar/building_blocks/map_tool_bar/SettingsBtn.vue';
 </script>
---
+++ src/components/solar/building_blocks/map_tool_bar/SaveBtn.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-btn icon>
+        <v-icon>mdi-file-document-plus-outline</v-icon>
+    </v-btn>
+</template>
+
+<script setup lang="ts">
+
+</script>
