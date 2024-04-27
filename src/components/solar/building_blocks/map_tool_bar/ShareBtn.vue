dd00f04de64004149a27e89bd14b9c98a0f74258
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-26 23:06:30 -0400

    Mathis fix: wrong use of i18n package

--- src/components/solar/building_blocks/map_tool_bar/ShareBtn.vue
+++ src/components/solar/building_blocks/map_tool_bar/ShareBtn.vue
@@ -1,16 +1,13 @@
 <template>
     <v-btn @click="copyRouteToClipboard()" icon>
         <v-icon>mdi-share-outline</v-icon>
-        <Tooltip :message="t(`solar.data-panel.header.toolbar.share-tooltip`)" />
+        <Tooltip :message="$t(`solar.data-panel.header.toolbar.share-tooltip`)" />
     </v-btn>
 </template>
 
 <script setup lang="ts">
 import Tooltip from "@/components/general/Tooltip.vue";
-import { useI18n } from "vue-i18n";
 import { copyToClipboard } from "@/helpers/util";
-
-const t = useI18n().t;
 
 function copyRouteToClipboard() {
     const fullCurrentRoute = window.location.href;
