89983e21ba9ab4179848ee15d0851f09eb95ccb3
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-05-15 18:31:41 -0400

    Mathis feat: Added PageSubtitleContainer component to unify pages

---
+++ src/components/general/PageSubtitleContainer.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="page-subtitle-container">
+        <div v-if="stringHasValue(props.pageSubtitlePrecontent)" class="page-subtitle-precontent">{{ props.pageSubtitlePrecontent }}</div>
+        <div v-if="stringHasValue(props.pageSubtitle)" class="page-subtitle">{{ props.pageSubtitle }}</div>
+        <div v-if="stringHasValue(props.pageSubtitlePrecision)" class="page-subtitle-precision">{{ props.pageSubtitlePrecision }}</div>
+        <div v-if="objectHasValue(props.pageSubtitleAction)" class="page-subtitle-action">
+            <v-card
+                @click="emits('onPageSubtitleActionClick')"
+                class="pa-6 rounded-lg mx-auto"
+                style="width: 250px"
+                elevation="2"
+                color="theme"
+            >
+                <v-row class="pa-0">
+                    <v-col class="pa-0" cols="9">
+                        <div style="font-weight: bold">{{ props.pageSubtitleAction?.text }}</div>
+                    </v-col>
+                    <v-col class="pa-0" cols="3" align-self="center">
+                        <v-icon>{{ props.pageSubtitleAction?.icon }}</v-icon>
+                    </v-col>
+                </v-row>
+            </v-card>
+        </div>
+
+        <!-- Add custom content -->
+        <div class="page-subtitle-content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+import { stringHasValue, objectHasValue } from '@/helpers/componentConditionals';
+
+const props = defineProps({
+    pageSubtitlePrecontent: {
+        type: String,
+        required: false
+    },
+    pageSubtitle: {
+        type: String,
+        required: true
+    },
+    pageSubtitlePrecision: {
+        type: String,
+        required: false
+    },
+    pageSubtitleAction: {
+        type: Object as PropType<PageSubtitleAction>,
+        required: false
+    }
+});
+
+const emits = defineEmits(["onPageSubtitleActionClick"]);
+
+interface PageSubtitleAction {
+    text: string,
+    icon: string
+};
+</script>
