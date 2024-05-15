4c6febaacda967eefc2f21dd779185683a1c9961
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-05-15 18:00:10 -0400

    Mathis feat: PageTitleContainer component to unify the way components are created.

---
+++ src/components/general/PageTitleContainer.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="page-title-container">
+        <div class="page-title-precontent">{{ props.pageTitlePrecontent }}</div>
+        <div class="page-title">{{ props.pageTitle }}</div>
+        <div class="page-title-precision">{{ props.pageTitlePrecision }}</div>
+        <div class="page-title-action">
+            <v-card
+                @click="emits('onPageTitleActionClick')"
+                class="pa-6 rounded-lg mx-auto"
+                style="width: 250px"
+                elevation="2"
+                color="theme"
+            >
+                <v-row class="pa-0">
+                    <v-col class="pa-0" cols="9">
+                        <div style="font-weight: bold">{{ props.pageTitleAction?.text }}</div>
+                    </v-col>
+                    <v-col class="pa-0" cols="3" align-self="center">
+                        <v-icon>{{ props.pageTitleAction?.icon }}</v-icon>
+                    </v-col>
+                </v-row>
+            </v-card>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+const props = defineProps({
+    pageTitlePrecontent: {
+        type: String,
+        required: false
+    },
+    pageTitle: {
+        type: String,
+        required: true
+    },
+    pageTitlePrecision: {
+        type: String,
+        required: false
+    },
+    pageTitleAction: {
+        type: Object as PropType<PageTitleAction>,
+        required: false
+    }
+});
+
+const emits = defineEmits(["onPageTitleActionClick"]);
+
+interface PageTitleAction {
+    text: string,
+    icon: string
+};
+</script>
