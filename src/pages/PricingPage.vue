acea8a01e98dedeb107f509fbda74760df86defb
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-05-17 19:08:56 -0400

    Mathis feat: Created pricing page and added to router

---
+++ src/pages/PricingPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <PageContainer>
+        <PageSection>
+            <PageTitleContainer
+                :pageTitle="'Pricing'"
+            />
+        </PageSection>
+    </PageContainer>
+</template>
+
+<script setup lang="ts">
+import PageContainer from '@/components/page_sections/PageContainer.vue';
+import PageSection from '@/components/page_sections/PageSection.vue';
+import PageTitleContainer from '@/components/page_sections/PageTitleContainer.vue';
+</script>
--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -46,6 +46,12 @@ const router = createRouter({
             beforeEnter: [authGuard]
         },
         {
+            path: "/pricing",
+            name: "pricing",
+            component: () => import("@/pages/PricingPage.vue"),
+            beforeEnter: []
+        },
+        {
             path: "/callback",
             name: "callback",
             component: () => import("@/pages/CallbackPage.vue"),
