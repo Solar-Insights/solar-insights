4ef917e2c8f2624645ef1548e512eaeb1f2b2da7
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-05-17 17:20:12 -0400

    Mathis feat: GetStartedPage 3rd section, still need 2nd. Has been i18ned.

--- src/pages/GetStartedPage.vue
+++ src/pages/GetStartedPage.vue
@@ -16,19 +16,55 @@
             >
                 <ShortDescriptionItemsContainer>
                     <ShortDescriptionItem
-                        :itemTitle="$t(`get-started.register-subtitle-container.short-items.invite.title`)"
-                        :itemContent="$t(`get-started.register-subtitle-container.short-items.invite.description`)"
+                        :itemTitle="$t(`get-started.register-subtitle-container.short-items.manage.title`)"
+                        :itemContent="$t(`get-started.register-subtitle-container.short-items.manage.description`)"
                         :itemIcon="`mdi-account-group-outline`"
                     />
                     <ShortDescriptionItem
-                    :itemTitle="$t(`get-started.register-subtitle-container.short-items.share.title`)"
+                        :itemTitle="$t(`get-started.register-subtitle-container.short-items.share.title`)"
                         :itemContent="$t(`get-started.register-subtitle-container.short-items.share.description`)"
                         :itemIcon="`mdi-share-outline`"
                     />
                     <ShortDescriptionItem
-                    :itemTitle="$t(`get-started.register-subtitle-container.short-items.save.title`)"
-                        :itemContent="$t(`get-started.register-subtitle-container.short-items.save.description`)"
-                        :itemIcon="`mdi-content-save-plus-outline`"
+                        :itemTitle="$t(`get-started.register-subtitle-container.short-items.help.title`)"
+                        :itemContent="$t(`get-started.register-subtitle-container.short-items.help.description`)"
+                        :itemIcon="`mdi-account-question-outline`"
+                    />
+                </ShortDescriptionItemsContainer>
+
+                <ParagraphContainer 
+                    class="text-center"
+                    :paragraphContent="$t(`get-started.register-subtitle-container.create-an-org-paragraph.content`)"
+                    :paragraphAction="{ text: $t(`get-started.register-subtitle-container.create-an-org-paragraph.action`), icon: '' }"
+                    @onParagraphActionClick="console.log('do something')"
+                />
+            </PageSubtitleContainer>
+        </PageSection>
+
+
+        <PageSection>
+            <PageSubtitleContainer
+                :pageSubtitle="$t(`get-started.use-solar-insights-subtitle-container.subtitle`)"
+                :pageSubtitlePrecision="$t(`get-started.use-solar-insights-subtitle-container.precision`)"
+            >
+            </PageSubtitleContainer>
+        </PageSection>
+
+        <PageSection>
+            <PageSubtitleContainer
+                :pageSubtitle="$t(`get-started.beyond-solar-insights-subtitle-container.subtitle`)"
+                :pageSubtitlePrecision="$t(`get-started.beyond-solar-insights-subtitle-container.precision`)"
+            >
+                <ShortDescriptionItemsContainer>
+                    <ShortDescriptionItem
+                        :itemTitle="$t(`get-started.beyond-solar-insights-subtitle-container.short-items.data.title`)"
+                        :itemContent="$t(`get-started.beyond-solar-insights-subtitle-container.short-items.data.description`)"
+                        :itemIcon="`mdi-database-outline`"
+                    />
+                    <ShortDescriptionItem
+                        :itemTitle="$t(`get-started.beyond-solar-insights-subtitle-container.short-items.graphs.title`)"
+                        :itemContent="$t(`get-started.beyond-solar-insights-subtitle-container.short-items.graphs.description`)"
+                        :itemIcon="`mdi-chart-line`"
                     />
                 </ShortDescriptionItemsContainer>
             </PageSubtitleContainer>
@@ -44,4 +80,5 @@ import PageSubtitleContainer from "@/components/page_sections/PageSubtitleContai
 import ShortDescriptionItem from "@/components/page_sections/ShortDescriptionItem.vue";
 import ShortDescriptionItemsContainer from "@/components/page_sections/ShortDescriptionItemsContainer.vue";
 import router from "@/plugins/router";
+import ParagraphContainer from "@/components/page_sections/ParagraphContainer.vue";
 </script>
