41453cb1d9373b04bce769376ddf02a26e663025
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-05-20 12:49:24 -0400

    Mathis feat: Added max and min props to imagecontainer + custom classes to mobile and pc

--- src/components/page_sections/ImageContainer.vue
+++ src/components/page_sections/ImageContainer.vue
@@ -2,10 +2,18 @@
     <v-img 
         class="hidden-xs pc-image-container"
         :src="props.pcImageSrc"
+        :max-width="props.maxWidth"
+        :min-width="props.minWidth"
+        :max-height="props.maxHeight"
+        :min-height="props.minHeight"
     />
     <v-img 
         class="hidden-sm-and-up mobile-image-container"
         :src="props.mobileImageSrc"
+        :max-width="props.maxWidth"
+        :min-width="props.minWidth"
+        :max-height="props.maxHeight"
+        :min-height="props.minHeight"
     />
 </template>
 
@@ -19,5 +27,21 @@ const props = defineProps({
         type: String,
         required: true
     },
+    maxWidth: {
+        type: Number,
+        required: false
+    },
+    maxHeight: {
+        type: Number,
+        required: false
+    },
+    minWidth: {
+        type: Number,
+        required: false
+    },
+    minHeight: {
+        type: Number,
+        required: false
+    }
 });
 </script>
