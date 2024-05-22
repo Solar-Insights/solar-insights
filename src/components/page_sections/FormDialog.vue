6227c9559d2b60b0093e436904b1179fced0defb
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-05-21 22:11:10 -0400

    Mathis feat: Form component helpers

---
+++ src/components/page_sections/FormDialog.vue
@@ -0,0 +1,71 @@
+<template>
+    <v-dialog v-model="dialog" activator="parent" max-width="600">
+        <v-card class="rounded-lg">
+            <v-card-title class="mt-3">
+                <div class="d-flex">
+                    <v-icon>{{ props.formIcon }}</v-icon>
+                    <div class="ml-4">{{ props.formTitle }}</div>
+                </div>
+            </v-card-title>
+
+            <v-card-text>
+                <slot></slot>
+            </v-card-text>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn @click="dialog = false" class="font-weight-medium" variant="flat">
+                    {{ props.formCancelButton }}
+                </v-btn>
+
+                <v-btn
+                    @click="emits(`validateForm`)"
+                    class="font-weight-medium"
+                    variant="flat"
+                    color="theme"
+                >
+                    {{ props.formConfirmButton }}
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from "vue";
+
+const props = defineProps({
+    formTitle: {
+        type: String,
+        required: true
+    },
+    formIcon: {
+        type: String,
+        required: true
+    },
+    formCancelButton: {
+        type: String,
+        required: true
+    },
+    formConfirmButton: {
+        type: String,
+        required: true
+    },
+    formClose: {
+        type: Boolean,
+        required: true
+    }
+});
+
+const emits = defineEmits(["validateForm", "formWasClosed"]);
+
+const dialog = ref<boolean>(false);
+
+watch(() => props.formClose, () => {
+    if (!props.formClose) return;
+
+    dialog.value = false;
+    emits("formWasClosed");
+})
+</script>
---
+++ src/components/page_sections/FormDialogSection.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-sheet class="form-dialog-section-container">
+        <div class="form-dialog-section-title">{{ props.sectionTitle }}</div>
+        <slot></slot>
+    </v-sheet>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    sectionTitle: {
+        type: String,
+        required: true
+    }
+});
+</script>
