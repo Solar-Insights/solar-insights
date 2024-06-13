48dfa7f8c6ddf75e32f3aa1bfc0eaf9fd4e38f91
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-06-12 20:18:40 -0400

    feat: billing item component to use in billing cards

---
+++ src/components/organization/my_organization_admin/billable_card/BillableItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-row class="billable-item-container">
+        <v-col class="billable-item-title" cols="9">
+            {{ title }}
+        </v-col>
+        <v-col class="billable-item-value" cols="3" align="center">
+            {{ value }}
+        </v-col>
+    </v-row>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    value: {
+        type: [String, Number],
+        required: true,
+    }
+});
+</script>
