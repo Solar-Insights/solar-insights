5e916bb053d9b2e0bbc8ee2e16a6d287e9891f94
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-06-12 20:18:54 -0400

    feat: billing card to contain data & billing items

---
+++ src/components/organization/my_organization_admin/billable_card/BillableCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-card class="billable-card" max-width="500" border>
+        <v-card-text class="billable-card-container">
+            <div class="billable-card-title"> {{ title }} </div>
+
+            <slot></slot>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    }
+});
+
+</script>
