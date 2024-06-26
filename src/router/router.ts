24842d0e4838918c9c7987e772a08c695b09cf53
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-06-25 21:44:52 -0400

    feat: finalized router changes

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { authGuard } from "@auth0/auth0-vue";
-import { ROUTE_INFOS } from "./routes";
+import { HOME, SEARCH, SOLAR_MAP, GET_STARTED, MY_ORGANIZATION, PRICING, CALLBACK, NOT_FOUND } from "@/router/routes";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,56 +9,56 @@ const router = createRouter({
     },
     routes: [
         {
-            path: ROUTE_INFOS["home"].en.path,
-            name: ROUTE_INFOS["home"].en.name,
+            path: HOME.en.path,
+            name: HOME.en.name,
             component: () => import("@/pages/HomePage.vue"),
             beforeEnter: []
         },
         {
-            path: ROUTE_INFOS["search"].en.path,
-            name: ROUTE_INFOS["search"].en.name,
+            path: SEARCH.en.path,
+            name: SEARCH.en.name,
             component: () => import("@/pages/SearchPage.vue"),
             beforeEnter: []
         },
         {
-            path: ROUTE_INFOS["solar-map"].en.path,
-            name: ROUTE_INFOS["solar-map"].en.name,
+            path: SOLAR_MAP.en.path,
+            name: SOLAR_MAP.en.name,
             component: () => import("@/pages/SolarMapPage.vue"),
             beforeEnter: [authGuard]
         },
         {
-            path: ROUTE_INFOS["get-started"].en.path,
-            name: ROUTE_INFOS["get-started"].en.name,
+            path: GET_STARTED.en.path,
+            name: GET_STARTED.en.name,
             component: () => import("@/pages/GetStartedPage.vue"),
             beforeEnter: []
         },
         {
-            path: ROUTE_INFOS["my-organization"].en.path,
-            name: ROUTE_INFOS["my-organization"].en.name,
+            path: MY_ORGANIZATION.en.path,
+            name: MY_ORGANIZATION.en.name,
             component: () => import("@/pages/MyOrganizationPage.vue"),
             beforeEnter: [authGuard]
         },
         {
-            path: ROUTE_INFOS["pricing"].en.path,
-            name: ROUTE_INFOS["pricing"].en.name,
+            path: PRICING.en.path,
+            name: PRICING.en.name,
             component: () => import("@/pages/PricingPage.vue"),
             beforeEnter: []
         },
         {
-            path: ROUTE_INFOS["callback"].en.path,
-            name: ROUTE_INFOS["callback"].en.name,
+            path: CALLBACK.en.path,
+            name: CALLBACK.en.name,
             component: () => import("@/pages/CallbackPage.vue"),
             beforeEnter: []
         },
         {
-            path: ROUTE_INFOS["not-found"].en.path,
-            name: ROUTE_INFOS["not-found"].en.name,
+            path: NOT_FOUND.en.path,
+            name: NOT_FOUND.en.name,
             component: () => import("@/pages/CatchAllPage.vue"),
             beforeEnter: []
         },
         {
             path: "/:catchAll(.*)",
-            redirect: "/not-found"
+            redirect: NOT_FOUND.en.path
         }
     ]
 });
