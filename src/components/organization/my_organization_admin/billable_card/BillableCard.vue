e7636dee4306cd1bf8306effef55febfd2cd3b0f
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2024-09-01 22:52:40 -0400

    fix: Smoll bug mentionned by Philippe regarding the spacing of the monthly usage cards

--- src/components/organization/my_organization_admin/billable_card/BillableCard.vue
+++ src/components/organization/my_organization_admin/billable_card/BillableCard.vue
@@ -1,6 +1,6 @@
 <template>
     <v-col cols="12" xs="12" :sm="fullWidth ? 12 : 6" :md="fullWidth ? 12 : 6" :class="fullWidth ? `px-0` : ``">
-        <v-card class="billable-card" :max-width="fullWidth ? `` : `600`">
+        <v-card class="billable-card">
             <v-card-text class="billable-card-container">
                 <div class="billable-card-title-container">
                     <div class="billable-card-title">
